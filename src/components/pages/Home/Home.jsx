57ee9e128f2412ceeaa2d2155afb4d41f0e65693
prasouk-ph <prasouk.ph@gmail.com>
2022-03-04 22:49:05 +0100

    Home page in progress

--- src/components/pages/Home/Home.jsx
+++ src/components/pages/Home/Home.jsx
@@ -1,4 +1,8 @@
 import './Home.css';
+import CaloriesIcon from '../../../assets/calories-icon.png'
+import ProteinsIcon from '../../../assets/protein-icon.png'
+import CarbsIcon from '../../../assets/carbs-icon.png'
+import FatIcon from '../../../assets/fat-icon.png'
 
 function Home() {
     return (
@@ -6,8 +10,47 @@ function Home() {
             <div className='home-header'>
                 <p className='hello'>Bonjour</p>
                 <p className='username'>Thomas</p>
+                <p className='cheering-message'>Félicitations ! Vous avez explosé vos objectifs hier 👏</p>
             </div>
-            <p className='cheering-message'>Félicitation ! Vous avez explosé vos objectifs hier 👏</p>
+
+            <main>
+                <div className="charts-container">
+                </div>
+
+                <div className="nutritional-intake">
+                    <div className="nutrient-item">
+                        <img className='nutrient-icon' src={CaloriesIcon} alt="calorie icon" />
+                        <div className="nutrient-details">
+                            <p className='nutrient-quantity'>1,930kCal</p>
+                            <p className='nutrient-name'>Calories</p>
+                        </div>
+                    </div>
+
+                    <div className="nutrient-item">
+                        <img className='nutrient-icon' src={ProteinsIcon} alt="protein icon" />
+                        <div className="nutrient-details">
+                            <p className='nutrient-quantity'>155g</p>
+                            <p className='nutrient-name'>Protéines</p>
+                        </div>
+                    </div>
+
+                    <div className="nutrient-item">
+                        <img className='nutrient-icon' src={CarbsIcon} alt="carbs icon" />
+                        <div className="nutrient-details">
+                            <p className='nutrient-quantity'>290g</p>
+                            <p className='nutrient-name'>Glucides</p>
+                        </div>
+                    </div>
+
+                    <div className="nutrient-item">
+                        <img className='nutrient-icon' src={FatIcon} alt="fat icon" />
+                        <div className="nutrient-details">
+                            <p className='nutrient-quantity'>50g</p>
+                            <p className='nutrient-name'>Lipides</p>
+                        </div>
+                    </div>
+                </div>
+            </main>
         </div>
     );
 }
