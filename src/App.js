90c7003b5328c68918995810e0e958e507fa3455
prasouk-ph <prasouk.ph@gmail.com>
2022-03-04 19:05:26 +0100

    Install react router

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import Layout from './components/Layout/Layout'
+import Home from './components/pages/Home/Home'
+import NotFoundPage from './components/pages/NotFound/NotFound'
+import './App.css';
+
+function App() {
+  return (
+    <Router>
+        <Routes>
+            <Route path="/" element={<Layout />}>
+                <Route index element={<Home />} />
+                <Route path="*" element={<NotFoundPage />}/>
+            </Route>
+        </Routes>
+    </Router>
+  );
+}
+
+export default App;
