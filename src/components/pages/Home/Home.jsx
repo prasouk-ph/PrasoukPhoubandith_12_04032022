563fe50d79cc4d9419a044028aafcdbcfa394646
prasouk-ph <prasouk.ph@gmail.com>
2022-03-07 16:16:07 +0100

    Add fetch to get username

--- src/components/pages/Home/Home.jsx
+++ src/components/pages/Home/Home.jsx
@@ -8,14 +8,49 @@ import SessionChart from './SessionChart/SessionChart'
 import CharateristicsChart from './CharateristicsChart/CharateristicsChart'
 import FatScore from './FatScore/FatScore'
 // import MyComponent from '../../../services/UserService'
+// import fetchData from '../../../services/api'
+import { useState, useEffect } from "react";
+
 
 function Home() {
+    const [error, setError] = useState(null);
+    const [isLoaded, setIsLoaded] = useState(false);
+    const [userData, setUserData] = useState({});
+
+    async function fetchData() {
+        setIsLoaded(true)
+        try {
+            const response = await fetch(`http://localhost:3000/user/12`)
+            if (!response.ok) {
+                throw new Error(
+                    `This is an HTTP error: The status is ${response.status}`
+                );
+            } else {
+                const { data } = await response.json()
+                setUserData(data)
+            }
+        } catch (error) {
+            console.log(error.message)
+            setError(true)
+        }
+        finally {
+            setIsLoaded(true)
+        }
+    }
+    
+    useEffect(() => {
+        fetchData()
+    }, [])
+
+    const userFirstName = userData?.userInfos?.firstName // without "?" React crashes cause wants to access the property before mounting, while the property has not yet received any content
+
+
     return (
         <div className='home'>
             <div className='home-header'>
                 <div className='greeting-message'>
                     <p className='hello'>Bonjour</p>
-                    <p className='username'>Thomas</p>
+                    <p className='username'>{userFirstName}</p>
                 </div>
                 <p className='cheering-message'>Félicitations ! Vous avez explosé vos objectifs hier 👏</p>
             </div>
